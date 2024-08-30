1d0dbeb6f4c1de2c1af610a9cd8b2299658ca926
arlsan00099 <itsfabulous2058@gmail.com>
2024-08-30 12:36:58 +0500

    testttt

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Arslan and new ');
+    res.send('Hello from Arslan ');
 });
 
 app.listen(port, () => {
